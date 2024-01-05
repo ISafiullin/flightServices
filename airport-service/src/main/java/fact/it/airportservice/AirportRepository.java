951f93fd0f5efe1f69f75481bf7a0d0cb5f961c2
Ilias <r0832668@student.thomasmore.be>
2024-01-05 19:42:07 +0100

    updated services

--- airport-service/src/main/java/fact/it/airportservice/AirportRepository.java
+++ airport-service/src/main/java/fact/it/airportservice/AirportRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface AirportRepository extends JpaRepository<Airport, Long> {
-    List<AirportLineFlight> findByFlightNumberIn(List<String> flightNumbers);
+    List<Airport> findByFlightRequestNumber(String flightRequestNumber);
 }
