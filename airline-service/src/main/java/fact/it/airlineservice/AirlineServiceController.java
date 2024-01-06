e5c534cdc223bb7bc09095600740860ca87fc8dd
Ilias <r0832668@student.thomasmore.be>
2024-01-06 15:38:05 +0100

    updated airport-service

--- airline-service/src/main/java/fact/it/airlineservice/AirlineServiceController.java
+++ airline-service/src/main/java/fact/it/airlineservice/AirlineServiceController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/airlines")
+@RequestMapping("/api/airline")
 @RequiredArgsConstructor
 public class AirlineServiceController {
     private final AirlineService airlineService;
